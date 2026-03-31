d87862d03f37ec71720bbfa294f21636d55119ed
RachidJedata <jedatarachid@gmail.com>
2026-03-31 11:20:00 +0100

    tt

--- src/app/(with-header)/layout.tsx
+++ src/app/(with-header)/layout.tsx
@@ -72,7 +72,7 @@ export default async function RootLayout({
       </div>
 
       <Footer />
-      {(session?.user) && (<AiChatButton />)}
+      {/* {(session?.user) && (<AiChatButton />)} */}
     </>
   );
 }
